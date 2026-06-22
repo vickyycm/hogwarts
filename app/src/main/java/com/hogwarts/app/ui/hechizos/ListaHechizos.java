2d8eeac6c5f1282cefa1c1a98be6cbd5928a484e
Victoria <victoria_cossimazza@outlook.com>
2026-06-22 00:02:01 -0300

    Creacion de vistas y codigo de hechizos

---
+++ app/src/main/java/com/hogwarts/app/ui/hechizos/ListaHechizos.java
@@ -0,0 +1,26 @@
+package com.hogwarts.app.ui.hechizos;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.hogwarts.app.R;
+
+public class ListaHechizos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_lista_hechizos);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/com/hogwarts/app/ui/hechizos/ListaHechizosAdapter.java
@@ -0,0 +1,4 @@
+package com.hogwarts.app.ui.hechizos;
+
+public class ListaHechizosAdapter {
+}
